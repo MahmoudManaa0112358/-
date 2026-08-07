cc5219cd3eebfcc21bd34e33bbd14fe2ea06bf9f
MahmoudManaa0112358 <mahmoudmanaa3424@gmail.com>
2026-08-08 00:41:27 +0300

    Add files via upload

---
+++ code/JS/data/other.js
@@ -0,0 +1,25 @@
+const other =[
+    {
+        name: "بنزينة التفريعة",
+        phon: "01223428281",
+        location: "https://www.google.com/maps/dir//68VG%2BJHX+%D8%A8%D9%86%D8%B2%D9%8A%D9%86%D8%A9+%D8%A7%D9%84%D8%AA%D9%81%D8%B1%D9%8A%D8%B9%D8%A9,+Port+Fouad+City,+Port+Said+Governorate+8587001%E2%80%AD/@31.243789,32.3268812,19.18z/data=!4m16!1m7!3m6!1s0x14f99db8be84f8d7:0xfdc73c27e74f0529!2z2KjZhtiy2YrZhtipINin2YTYqtmB2LHZiti52Kk!8m2!3d31.2441114!4d32.3264793!16s%2Fg%2F1ptwk0k14!4m7!1m0!1m5!1m1!1s0x14f99db8be84f8d7:0xfdc73c27e74f0529!2m2!1d32.3264793!2d31.2441114?entry=ttu&g_ep=EgoyMDI2MDYyMy4wIKXMDSoASAFQAw%3D%3D"
+    },
+    {
+        name: "هلنان",
+        phon: "01279555521",
+        location: "https://www.google.com/maps/dir//Helnan+Port+Fouad+Hotel+%D9%81%D9%86%D8%AF%D9%82+%D9%87%D9%84%D9%86%D8%A7%D9%86+%D8%A8%D9%88%D8%B1%D9%81%D8%A4%D8%A7%D8%AF,+23-Jul,+st%D8%8C+Port+Fouad+City,+Port+Said+Governorate+8587010%E2%80%AD/@31.2413521,32.3321348,19.38z/data=!4m19!1m10!3m9!1s0x14f99d9c4473397f:0x84ae40fa760ebd93!2zSGVsbmFuIFBvcnQgRm91YWQgSG90ZWwg2YHZhtiv2YIg2YfZhNmG2KfZhiDYqNmI2LHZgdik2KfYrw!5m2!4m1!1i2!8m2!3d31.2417355!4d32.33247!16s%2Fg%2F11qnt583bl!4m7!1m0!1m5!1m1!1s0x14f99d9c4473397f:0x84ae40fa760ebd93!2m2!1d32.33247!2d31.2417355?entry=ttu&g_ep=EgoyMDI2MDYxMC4wIKXMDSoASAFQAw%3D%3D",
+    },
+    {
+        name: "جيم جراند ريفينج",
+        phon: "0121273337",
+        location: "https://www.google.com/maps/dir//Grand+Revenge+Gym,+Helnan+hotel,+Port+Fouad+City,+Port+Said+Governorate+8587010/@31.2415688,32.3314535,18.37z/data=!4m16!1m7!3m6!1s0x14f99dc611c2b7b9:0xb15c3db3bf97f40e!2sGrand+Revenge+Gym!8m2!3d31.2415647!4d32.3321303!16s%2Fg%2F11k3w5pxgw!4m7!1m0!1m5!1m1!1s0x14f99dc611c2b7b9:0xb15c3db3bf97f40e!2m2!1d32.3321303!2d31.2415647?entry=ttu&g_ep=EgoyMDI2MDYyOS4wIKXMDSoASAFQAw%3D%3D",
+    },
+]
+/*
+    {
+        name: "",
+        phon: "",
+        location: "",
+    },
+*/
+export {other};
