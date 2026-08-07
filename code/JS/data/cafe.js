8246acba54f3f1a113aab4691906bbb66a2abfec
MahmoudManaa0112358 <mahmoudmanaa3424@gmail.com>
2026-08-08 00:40:59 +0300

    Add cafe data with details and images

---
+++ code/JS/data/cafe.js
@@ -0,0 +1,47 @@
+const cafe =[
+    {
+        name: "بلاك فوكس",
+        phon: "01201799105",
+        location: "https://www.google.com/maps/dir//Black+Fox,+Port+Fouad+City,+Port+Said+Governorate+8587010/@31.2413521,32.3321348,19.38z/data=!4m16!1m7!3m6!1s0x14f99d004188bcc5:0xb81a4838ca959f21!2sBlack+Fox!8m2!3d31.2415298!4d32.3319189!16s%2Fg%2F11vr8yq7yf!4m7!1m0!1m5!1m1!1s0x14f99d004188bcc5:0xb81a4838ca959f21!2m2!1d32.3319421!2d31.2415686?entry=ttu&g_ep=EgoyMDI2MDYxMC4wIKXMDSoASAFQAw%3D%3D",
+        imge1: "../../imgs/بلاك فوكس/img1.jpg",
+        imge2: "../../imgs/بلاك فوكس/img2.jpg",
+        imge3: "../../imgs/بلاك فوكس/img3.jpg",
+    },
+    {
+        name: "جلال",
+        phon: "01009580830",
+        location: "https://www.google.com/maps/dir//Galal+Coffee,+%D9%8A%D9%88%D9%84%D9%8A%D9%88-23%D8%8C+%D8%A8%D9%88%D8%B1+%D9%81%D8%A4%D8%A7%D8%AF%D8%8C+%D8%A8%D9%88%D8%B1+%D8%B3%D8%B9%D9%8A%D8%AF%D8%8C%D8%8C,+Port+Fouad+City,+Port+Said+Governorate%E2%80%AD/@31.2404091,32.3337828,19.72z/data=!4m16!1m7!3m6!1s0x14f99d23c7df8a69:0xdbdda9759477ce69!2sGalal+Coffee!8m2!3d31.2405672!4d32.3339068!16s%2Fg%2F11smkb3yhw!4m7!1m0!1m5!1m1!1s0x14f99d23c7df8a69:0xdbdda9759477ce69!2m2!1d32.3339266!2d31.2406002?entry=ttu&g_ep=EgoyMDI2MDYxMC4wIKXMDSoASAFQAw%3D%3D",
+        imge1:"../../imgs/جلال/img1.jpg",
+        imge2:"../../imgs/جلال/img2.jpg",
+        imge3:"../../imgs/جلال/img3.jpg",
+        imge4:"../../imgs/جلال/img4.jpg",
+    },
+    {
+        name: "اسطنبول",
+        phon: "01222134674",
+        location: "https://www.google.com/maps/dir//68RM%2BF2V+Istanbul+caf%C3%A9,+23-Jul,+Port+Fouad+City,+Port+Said+Governorate+8587010/@31.2411987,32.3325727,20.53z/data=!4m16!1m7!3m6!1s0x14f99de6c0c1218b:0x5a46b9f347768e6e!2sIstanbul+caf%C3%A9!8m2!3d31.2412321!4d32.3325519!16s%2Fg%2F11y3yjh_yq!4m7!1m0!1m5!1m1!1s0x14f99de6c0c1218b:0x5a46b9f347768e6e!2m2!1d32.3325689!2d31.2412605?entry=ttu&g_ep=EgoyMDI2MDYxMC4wIKXMDSoASAFQAw%3D%3D",
+        imge1:"../../imgs/كافيه اسطنبول/img1.jpg",
+        imge2:"../../imgs/كافيه اسطنبول/img2.jpg",
+        imge3:"../../imgs/كافيه اسطنبول/img3.jpg",
+        imge4:"../../imgs/كافيه اسطنبول/img4.jpg",
+        imge5:"../../imgs/كافيه اسطنبول/img5.jpg",
+    },
+    {
+        name: "كاساتا مورا",
+        phon: "01111050606",
+        location: "https://www.google.com/maps/dir//%D9%83%D8%A7%D8%B3%D8%A7%D8%AA%D8%A7+%D9%85%D9%88%D8%B1%D8%A7%D8%8C+,+%D9%85%D8%B3%D8%A7%D9%83%D9%86+%D8%A7%D8%A8%D9%88+%D8%A8%D9%83%D8%B1+,+%D8%B7%D8%B1%D9%8A%D9%82+%D8%A7%D9%84%D9%85%D9%84%D8%A7%D8%AD%D8%A9,+%D8%A8%D9%88%D8%B1+%D9%81%D8%A4%D8%A7%D8%AF%D8%8C+%D9%85%D8%AD%D8%A7%D9%81%D8%B8%D8%A9+%D8%A8%D9%88%D8%B1%D8%B3%D8%B9%D9%8A%D8%AF+8584231%D8%8C+%D8%A8%D8%A7%D9%84%D9%82%D8%B1%D8%A8+%D9%85%D9%86+%D8%A8%D9%86%D8%B2%D9%8A%D9%86%D9%87+%D8%A7%D9%84%D8%AA%D9%81%D8%B1%D9%8A%D8%B9%D8%A9%D8%8C+%D8%A8%D9%88%D8%B1%D9%81%D8%A4%D8%A7%D8%AF,+Port+Said+Governorate+8584231%E2%80%AD/@31.2436699,32.3252482,20z/data=!4m16!1m7!3m6!1s0x14f99d5b9e1023ed:0xf86ee4741119b124!2z2YPYp9iz2KfYqtinINmF2YjYsdin!8m2!3d31.2436698!4d32.3255612!16s%2Fg%2F11n4xpf911!4m7!1m0!1m5!1m1!1s0x14f99d5b9e1023ed:0xf86ee4741119b124!2m2!1d32.3255612!2d31.2436698?entry=ttu&g_ep=EgoyMDI2MDYyOS4wIKXMDSoASAFQAw%3D%3D",
+        imge1: "../../imgs/كاساتا مورا/img1.jpg",
+        imge2: "../../imgs/كاساتا مورا/img2.jpg",
+        imge3: "../../imgs/كاساتا مورا/img3.jpg",
+    },
+]
+/*
+    {
+        name: "",
+        phon: "",
+        location: "",
+        imge1: "",
+    },
+*/
+
+export {cafe};
